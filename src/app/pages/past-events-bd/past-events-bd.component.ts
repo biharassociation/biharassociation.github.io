dd9677f4f3ca5aaf49777bbccbcf520e41897f28
Atharv Gupta <atharv.gupta124@gmail.com>
2023-07-23 18:30:25 -0700

    updated events for float

---
+++ src/app/pages/past-events-bd/past-events-bd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-past-events-bd',
+  templateUrl: './past-events-bd.component.html',
+  styleUrls: ['./past-events-bd.component.css']
+})
+export class PastEventsBdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
